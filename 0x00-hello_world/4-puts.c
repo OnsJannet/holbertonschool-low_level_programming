61ccafe1cda29e592aea76f65e3da424a3c0426e
onsjannet <onsjannet@gmail.com>
2020-06-10 18:01:24 +0100

    changes in file 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,8 @@
-#!/bin/bash
-puts("with proper grammar, but the outcome is a piece of art,") return 0;
+#include <stdio.h>
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+  return 0;
+}
+
