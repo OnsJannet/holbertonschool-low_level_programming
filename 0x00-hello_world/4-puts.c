60245f9b02d3d81ae302921444de0498919c6096
onsjannet <onsjannet@gmail.com>
2020-06-10 18:06:45 +0100

    changed 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- *main -void
+ * main - Entry point
  *
- Return: 0
-*/
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+
+return (0);
 }
 
