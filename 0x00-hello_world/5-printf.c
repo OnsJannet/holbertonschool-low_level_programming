a665abcd8fba688d4b0ba4260b3f4ac1d8472394
onsjannet <onsjannet@gmail.com>
2020-06-10 18:15:10 +0100

    changes to the file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
+
 /**
- *
- * main - Entry point
+ * main - void
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-  
- printf("with proper grammar, but the outcome is a piece of art,");
- 
- return (0);
- 
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
