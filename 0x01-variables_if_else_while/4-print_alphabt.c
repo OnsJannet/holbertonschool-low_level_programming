0cf70f11c11e94dd4c15467a02d84a5b216fc3ae
onsjannet <onsjannet@gmail.com>
2020-06-11 19:38:19 +0100

    added a new file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - maine function
+*Return: 0
+*/
+int main(void)
+{
+	char p;
+
+	for (ap = 'a'; i - 1 < 'z'; ++ap)
+	{
+		if ((ap != 'q') && (ap != 'e'))
+		{
+			putchar(ap);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
