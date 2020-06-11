3ea643162304848e855ec9bdec1d4d41374c6d71
onsjannet <onsjannet@gmail.com>
2020-06-11 10:10:25 +0100

    created a new file  0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main () {
+  
+  /*local variable definition*/
+  int n 
+
+  /* positive*/
+    
+    if ( n > 0 ) {
+      printf("is Positive")
+	}
+  /* zero*/
+  
+    else if {
+      (n = 0 )
+	printf("zero")
+	}
+  /* negative*/
+  
+    else {
+      (n < 0)
+	printf("is negative")
+	}
+  
+  return 0;
+  
+	
+  
+  
