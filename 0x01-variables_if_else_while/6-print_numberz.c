c75c645485c19305c7a78e3539738e924e71c6af
onsjannet <onsjannet@gmail.com>
2020-06-11 20:09:49 +0100

    added a new file and chmod

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - main function
+ *Return: return value zero (success)
+*/
+int main(void)
+{
+int character;
+for (character = '0'; character <= '9'; character++)
+putchar(character);
+putchar('\n');
+return (0);
+}
