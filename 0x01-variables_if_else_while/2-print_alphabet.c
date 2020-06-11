01d78c28566313f810e4cf65ec5474e3de0aaef4
onsjannet <onsjannet@gmail.com>
2020-06-11 18:53:57 +0100

    added a new file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - maine function
+*Return: 0
+*/
+int main(void)
+{
+	char ap;
+
+	for (ap = 'a'; ap <= 'z'; ++ap)
+	{
+		putchar(ap);
+	}
+	putchar('\n');
+	return (0);
+}
