38bd55ef4815b88f11c7ab4d60d296ccfada08c9
onsjannet <onsjannet@gmail.com>
2020-06-11 19:51:37 +0100

    added a new file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - maine function
+*Return: 0
+*/
+int main(void)
+{
+	unsigned int num;
+
+	for (num = '0'; num <= '9'; ++ap)
+	{
+		printf("%num", num)
+	}
+	putchar('\n');
+	return (0);
+}
