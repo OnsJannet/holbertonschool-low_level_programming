8b2df627963c248f8d98f5a8df6b83045a597a55
onsjannet <onsjannet@gmail.com>
2020-06-16 18:31:42 +0100

    cleaned code

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,19 +5,18 @@
 */
 void jack_bauer(void)
 {
-  int hours;
-  int minutes;
+int hours;
+int minutes;
 for (hours = 0 ; hours <= 23; hours++)
 {
 for (minutes = 0 ; minutes <= 59; minutes++)
-
 {
-  _putchar(hours / 10 + '0');
-  _putchar(hours % 10 + '0');
-  _putchar(':');
-  _putchar(minutes / 10 + '0');
-  _putchar(minutes % 10 + '0');
-  _putchar('\n');
+_putchar(hours / 10 + '0');
+_putchar(hours % 10 + '0');
+_putchar(':');
+_putchar(minutes / 10 + '0');
+_putchar(minutes % 10 + '0');
+_putchar('\n');
 }
 }
 }
