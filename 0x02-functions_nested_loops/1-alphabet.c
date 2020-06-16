49098cec680d082d920d3a23c88d5837a1172c33
onsjannet <onsjannet@gmail.com>
2020-06-16 07:44:46 +0100

    changes to putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,14 @@
 #include "holberton.h"
 
 /**
- *main - Entry point
- *
-Return: Always 0 (Success)
-*/
-
-void print_alphabet(void);
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
 {
-int i;
-for (i = 'a'; i <= 'z'; ++i)
-{
-_putchar(i);
-}
-_putchar('\n');
+	char l;
 
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+
+	_putchar('\n');
 }
