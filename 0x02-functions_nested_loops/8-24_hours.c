18e504ab0e1789ca09d93806e1662f4bc9e43ea4
onsjannet <onsjannet@gmail.com>
2020-06-16 12:05:33 +0100

    added a new file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,3 +1,27 @@
 #include "holberton.h"
-
-for
+/**
+*jack_bauer-  function that prints every minute of the day of Jack Bauer.
+*Return: Always 0.
+*/
+void jack_bauer(void)
+{
+int h, m, s, x;
+for (h = '0' ; h = '2'; h++)
+{
+for (m = '0' ; m = '9'; m++)
+}
+{
+for (s = '0' ; s = '5'; s++)
+}
+{
+for (x = '0' ; x = '9' ; x++)
+}
+if (h == 2 && m >= 4)
+{
+if (h != 2 || m < 4)
+}
+putchar(h)
+putchar(m)
+putchar(s)
+putcher(x)
+}
