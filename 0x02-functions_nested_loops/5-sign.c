0dc050b793349df47f176e9463927b6ef33fedb6
onsjannet <onsjannet@gmail.com>
2020-06-16 09:47:41 +0100

    cleaned line

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,10 @@
 #include "holberton.h"
-int print_sign(int n);
+/**
+ * print_sign - function that prints the sign of a number.
+ *@c: The number to be checked
+ * Return: Always 0.
+ */
+int print_sign(int n)
 {
 	if (n == 0)
 	{
