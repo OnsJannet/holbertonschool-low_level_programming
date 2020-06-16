526b2d6b634caa57af2d75a1de653d5931697be3
onsjannet <onsjannet@gmail.com>
2020-06-16 18:44:18 +0100

    added a new file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - add two integers and return the result
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of integers
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
