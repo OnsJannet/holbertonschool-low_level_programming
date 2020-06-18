b1fb0d304c6b5db3d2c1774539794ea4e180485b
onsjannet <onsjannet@gmail.com>
2020-06-18 18:33:48 +0100

    changed putchar to _putchar

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,17 +2,16 @@
 /**
 *more_numbers - print 10 times the numbers.
 *
-* Return: 0
 */
 void more_numbers(void)
 {
 int a, b;
-for (b = 0 ; b <= 10 ; b++)
+for (b = 0 ; b <= 9 ; b++)
 {
 for (a = 0 ; a <= 14 ; a++)
 {
-putchar(a / 10 + '0');
-putchar(a % 10 + '0');
+_putchar(a / 10 + '0');
+_putchar(a % 10 + '0');
 }
 putchar('\n');
 }
