d1f13acd5eddee63a073097897031b9e837aca85
onsjannet <onsjannet@gmail.com>
2020-06-18 18:04:04 +0100

    corrected code

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 /**
-*print_most_numbers(void) - function that prints most numbers.
+*print_most_numbers - function that prints most numbers.
 * Return: Always 0.
 */
 void print_most_numbers(void)
 {
 char a;
- for (a = '0' ; a <= '9'; a++)
-   {
-     if (a != 50 && a != 52)
+for (a = '0' ; a <= '9'; a++)
 {
- _putchar(a);
+if (a != 50 && a != 52)
+{
+_putchar(a);
 }
 }
 _putchar ('\n');
