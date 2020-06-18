67315b47c497345c4e0f3e7c257ec1c142507160
onsjannet <onsjannet@gmail.com>
2020-06-18 11:04:09 +0100

    worte the code for 3rd file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+*print_numbers(int n)- that prints the numbers.
+*Return: Always 0.
+*/
+void print_numbers(void);
+{
+char a;
+for (a = '0'; a <= '9'; ++a)
+{
+_putchar(a);
+}
+}
