6606ecd850babf0b02feec8aca32f0cad5df5d47
onsjannet <onsjannet@gmail.com>
2020-06-22 18:42:48 +0100

    changed code

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,13 @@
 #include "holberton.h"
 #include "stdio.h"
 /**
- * void reset_to_98(int *n) - takes a pointer to an int as parameter and updates the value it points
- * @n: integer
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+* reset_to_98 - takes a pointer to an int.
+* @n: integer
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
 void reset_to_98(int *n)
-int n =98;
-int *ptr;
-ptr = &n;
-printf("%d" , *ptr;)
+{
+*n = 98;
+}
