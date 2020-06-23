958507fd16946e19f64c60dbf6ac3a62be2b7a23
onsjannet <onsjannet@gmail.com>
2020-06-23 07:33:41 +0100

    added code

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+int i, j = 0;
+char temp;
+while (s[j] != '\0')
+j++
+j--;
+for (i = 0 ; i < j ; j--, i++)
+temp = s[i];
+s[i] = s[j];
+s[j] = temp;
+}
+}
