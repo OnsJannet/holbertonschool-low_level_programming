23515f36f3f51fe6fce6b028886bc2b157ee5d65
onsjannet <onsjannet@gmail.com>
2020-06-23 17:56:38 +0100

    changes to code

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,13 +10,17 @@
 
 void print_array(int *a, int n)
 {
-	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		printf("%d", a[i]);
-		if (i != n - 1)
-			printf(", ");
-	}
-	printf("\n");
+int i;
+for (i = 0; i < n; i++)
+{
+if (i != n - 1)
+{
+printf("%d, ", a[i]);
+}
+else
+{
+printf("%d", a[i]);
+}
+}
+printf("\n");
 }
