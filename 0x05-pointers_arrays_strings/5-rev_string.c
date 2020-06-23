c6711f41a9ee21780c651961040bb6ebdab6af7e
onsjannet <onsjannet@gmail.com>
2020-06-23 09:18:59 +0100

    corrected code

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,6 +10,7 @@ void rev_string(char *s)
 int i, j = 0;
 char temp;
 while (s[j] != '\0')
+{
 j++
 j--;
 for (i = 0 ; i < j ; j--, i++)
@@ -17,5 +18,6 @@ for (i = 0 ; i < j ; j--, i++)
 temp = s[i];
 s[i] = s[j];
 s[j] = temp;
+}
 }
 }
