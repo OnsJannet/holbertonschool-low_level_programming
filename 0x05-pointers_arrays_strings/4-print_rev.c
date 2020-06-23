90e5616a25ec6febbd03e3de6081c1b9858be246
onsjannet <onsjannet@gmail.com>
2020-06-23 07:25:26 +0100

    added code

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_rev - prints string in reverse
+ * @s: string to print
+ *
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+int i = 0, j;
+while (i != '\0')
+{
+i++;
+i--;
+for (j = i ; j != -1 ; j--)
+{
+_putchar(s[j]);
+_putchar('\n');
+}
