c7f03f6e4fe66238a189a8d01a9007d1fd9bfd97
onsjannet <onsjannet@gmail.com>
2020-06-25 19:50:39 +0100

    added file and code

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -10,6 +10,7 @@ void reverse_array(int *a, int n);
 char *cap_string(char *);
 char *rot13(char *);
 void print_number(int n);
+char *leet(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 #endif
