36950067de7064602dac0ca1886c569594cf9037
onsjannet <onsjannet@gmail.com>
2020-06-25 12:21:54 +0100

    added code

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,23 +1,15 @@
 #include "holberton.h"
 /**
-*reverse_array- is a function that reverses array of integers.
-* @*a: int.
-* @n: int.
-* @a: int.
-*
-*Return: an integer
-*
-**/
+ * reverse_array - reverses array of integers
+ * @a: input array
+ * @n: length of array
+ *
+ * Return: void
+ */
 void reverse_array(int *a, int n)
 {
-int start;
-int temp;
-if (start >= n)
-{
-return;
-int temp = start;
-int start = n;
-int n = temp;
-reverse_array(*a, start + 1, n - 1)
-}
+	int j, temp, i = n;
+
+	for (i--, j = 0; j < n / 2; j++, i--)
+		temp = a[j], a[j] = a[i], a[i] = temp;
 }
