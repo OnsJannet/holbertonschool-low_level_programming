cd817b173575853f259606f0b26cd735eb4ca9d9
onsjannet <onsjannet@gmail.com>
2020-06-25 18:14:52 +0100

    added a file + code troupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * string_toupper - converts string lowercase letters to uppercase
+ * @s: input string to be converted
+ *
+ * Return: upper string
+ */
+char *string_toupper(char *s)
+{
+
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] > 'a' && s[i] < 'z')
+			s[i] -= 32;
+	}
+	return (s);
+}
