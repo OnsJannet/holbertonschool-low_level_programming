37cb9929f7f49b5752fc7b3263a8a7b80040c8cd
onsjannet <onsjannet@gmail.com>
2020-06-25 08:11:06 +0100

    added a new file + code

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,21 +1,21 @@
 #include "holberton.h"
 /**
-*_strcat- is a function that concatenates two strings.
+*_strncat- is a function that concatenates two strings.
 * @src: 1st char
 * @dest: 2nd char
+* @n: and int
 *
 *Return: dest.
 *
 **/
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, j;
 
 	while (dest[i] != '\0')
 		i++;
-	for (j = 0; src[j] != '\0'; j++, i++)
+	for (j = 0; j < n && src[j] != '\0'; j++, i++)
 		dest[i] = src[j];
 	dest[i] = '\0';
 	return (dest);
-	
 }
