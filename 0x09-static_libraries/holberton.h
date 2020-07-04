2377b86427718561988c3a8c8d2fe61d27fd8ea9
onsjannet <onsjannet@gmail.com>
2020-07-04 19:20:52 +0100

    solving problems

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,5 +1,5 @@
-#ifndef HEADERFILE_H
-#define HEADERFILE_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,4 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
+#endif 
