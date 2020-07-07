be45c595cdc4c1fce482ce696d72e721f6a90965
onsjannet <onsjannet@gmail.com>
2020-07-07 16:49:00 +0100

    changed script

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,26 +1,17 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "holberton.h"
 /**
- * free_grid - Frees a 2 dimensional grid.
- *
- * @grid: Matrix.
- * @height: height.
- *
- * Return: Returns the created array.
-**/
-
+ * free_grid - frees a 2D grid previously created
+ * @grid: input pointer to grid or 2D array
+ * @height: height of 2D arrray
+ * Return: void
+ */
 void free_grid(int **grid, int height)
 {
-
-int i;
+	int i;
 
 	for (i = 0; i < height; i++)
 		free(grid[i]);
-
-free(grid);
-
-return;
-
+	free(grid);
 }
