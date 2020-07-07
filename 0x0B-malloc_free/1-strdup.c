5fcfc24013a5f3f6da3e21a6e2e96e838d76ba5e
onsjannet <onsjannet@gmail.com>
2020-07-07 16:29:20 +0100

    changed script

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,17 +8,29 @@
  */
 char *_strdup(char *str)
 {
-	char *pstr;
-	unsigned int i = 0;
+	char *s;
+	unsigned int i;
+	unsigned int size;
 
+	size = 0;
+	i = 0;
 	if (str == 0)
-		return (0);
-	while (str[i] != 0)
-		i++;
-	i++;
-	pstr = malloc(sizeof(char) * i);
-	for (i = 0; str[i] != 0; i++)
-		pstr[i] = str[i];
-	pstr[i] = 0;
-	return (pstr);
-}
+		return (NULL);
+	while (str[size] != '\0')
+	{
+		size++;
+
+	}
+		size++;
+		s = malloc(sizeof(char) * size);
+		if (s == 0)
+			return (NULL);
+			while (i < size)
+			{
+				s[i] = str[i];
+				i++;
+			}
+			s[i] = '\0';
+			return (s);
+		}
+		
