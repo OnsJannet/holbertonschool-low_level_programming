61432da813c407f529e58daea28dd9de239a57c0
onsjannet <onsjannet@gmail.com>
2020-07-13 16:32:41 +0100

    added files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Dog data.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Name of the owner.
+ * Description: A structure with the name, age and owner of a dog.
+**/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+#endif
