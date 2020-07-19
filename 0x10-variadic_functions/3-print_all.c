5bcdb435d76f804b915d492bd04705d965408402
onsjannet <onsjannet@gmail.com>
2020-07-19 13:10:32 +0100

    changed script

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,54 +1,51 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
-
 /**
  * print_all - prints anything
- * @format: any variable type
- * Return: Nothing
-**/
-
+ * @format: list of types of args passed to the func
+ * Return: void
+ */
 void print_all(const char * const format, ...)
 {
-unsigned int i = 0, j = 0;
-char *str;
-va_list ls;
-va_start(ls, format);
+	va_list list;
+	unsigned int i = 0, j = 0;
+	char *s;
 
-while (format && format[i])
-{
-	switch (format[i])
+	va_start(list, format);
+	while (format && format[i])
 	{
-		case 'c':
-			printf("%c", va_arg(ls, int));
-			break;
-		case 'i':
-			printf("%i", va_arg(ls, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(ls, double));
-			break;
-		case 's':
-			str = va_arg(ls, char *);
-			if (str == NULL)
-				str = "(nil)";
-
-			printf("%s", str);
-			break;
-	}
-j = i + 1;
-
-	while (format[j] && (format[j] == 'c' || format[j] == 'i' ||
-format[j] == 'f' || format[j] == 's'))
-	{
-		printf(", ");
-		break;
-	}
-
-i++;
-}
-printf("\n");
-va_end(ls);
-return;
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(list, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(list, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(list, double));
+				break;
+			case 's':
+				s = va_arg(list, char *);
+				if (s)
+				{
+					printf("%s", s);
+					break;
+				}
+				printf("(nil)");
+				break;
+		} j = i + 1;
+		while (format[j] && (format[i] == 'c' ||
+			 format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
+		{
+			if (format[j] == 'c' || format[j] == 'i' ||
+			 format[j] == 's' || format[i] == 'f')
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
+		i++;
+	} printf("\n");
 }
