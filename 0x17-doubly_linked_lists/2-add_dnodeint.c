9d199a26382d47c8dcf36390ce3e79abfaefac0c
onsjannet <onsjannet@gmail.com>
2020-09-06 19:45:02 +0100

    added script

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * add_dnodeint - a function that adds a new node
+ *@head: list
+ *@n : node
+ * Return: adresse or null
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL || head == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new_node;
+	*head = new_node;
+	return (new_node);
+}
