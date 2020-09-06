9f858d3e230e4f41311ec0539a1c857e6fa69801
onsjannet <onsjannet@gmail.com>
2020-09-06 17:33:27 +0100

    corrected betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,11 +7,10 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i;
- 
-for (i = 0; h!= NULL; i++)
+for (i = 0; h != NULL; i++)
 {
 	printf("%d\n", h->n);
-	h = h ->next;
+	h = h->next;
 }
 return (i);
 }
