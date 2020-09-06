f45e2a7d5c71419850db4e8aa7031107767f1cbd
onsjannet <onsjannet@gmail.com>
2020-09-06 17:38:53 +0100

    added script same script as before but only prints number of elements now

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,1 +1,15 @@
-
+#include "lists.h"
+/**
+ * dlistint_len - a function that returns the elements in a list
+ *@h: Pointer
+ * Return: number of elemnts
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i;
+for (i = 0; h != NULL; i++)
+{
+	h = h->next;
+}
+return (i);
+}
