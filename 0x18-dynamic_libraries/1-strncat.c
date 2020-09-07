86ba470dd11e0bb0895736299100597462e00883
onsjannet <onsjannet@gmail.com>
2020-09-07 18:17:57 +0100

    created a dynamic library

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+*_strncat- is a function that concatenates two strings.
+* @src: 1st char
+* @dest: 2nd char
+* @n: int
+*
+*Return: dest.
+*
+**/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j;
+
+	while (dest[i] != '\0')
+		i++;
+	for (j = 0; j < n && src[j] != '\0'; j++, i++)
+		dest[i] = src[j];
+	dest[i] = '\0';
+	return (dest);
+}
