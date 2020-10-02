91f9ed849fb90bb83b1eadcefc2499f79c3c9ed8
onsjannet <onsjannet@gmail.com>
2020-10-02 19:47:35 +0100

    corrected betty

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -4,7 +4,6 @@
  * hash_table_get -retrieves a value associated with a key
  * @ht: the hashtable in which to search for the data
  * @key: the key to search the hashtable for.
-
  * Return: The value that corresponds to the key if it is found, and NULL
  * otherwise. if the hashtable is null, return Null.
  */
