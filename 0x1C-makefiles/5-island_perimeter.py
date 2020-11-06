e550fced6302466c995cd2816bf944b68aedca1b
onsjannet <onsjannet@gmail.com>
2020-11-06 21:34:03 +0100

    changed methode

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 """Defines an island perimeter."""
+
+
 def island_perimeter(grid):
-        perimeter = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j]==1:
-                    perimeter +=4
-                    if i>0 and grid[i-1][j]==1:
-                        perimeter-=2
-                    if j>0 and grid[i][j-1]==1:
-                        perimeter-=2
-        return perimeter
+    perimeter = 0
+    i, j = len(grid), len(grid[0])
+    for row in range(0, i):
+        for col in range(0, j):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                if row == i - 1 or grid[row + 1][col] == 0:
+                    perimeter += 1
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                if col == i - 1 or grid[row][col + 1] == 0:
+                    perimeter += 1
+    return perimeter
