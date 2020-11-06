cba4d1b73cc4d581730f7ee6e532a97fd10cb07c
onsjannet <onsjannet@gmail.com>
2020-11-06 21:49:46 +0100

    pep8

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,17 +8,13 @@ def island_perimeter(grid):
     i, j = len(grid), len(grid[0])
     for row in range(0, i):
         for col in range(0, j):
-            #UP
             if grid[row][col] == 1:
                 if row == 0 or grid[row - 1][col] == 0:
                     perimeter += 1
-            #BOT
                 if row == i - 1 or grid[row + 1][col] == 0:
                     perimeter += 1
-            #LEFT
                 if col == 0 or grid[row][col - 1] == 0:
                     perimeter += 1
-            #RIGHT
                 if col == i - 1 or grid[row][col + 1] == 0:
                     perimeter += 1
     return perimeter
